f7391a7d2675aab361947b583636faa3dfa04013
Ernest Yeung <ernestyalumni@gmail.com>
2025-02-01 06:53:18 -0800

    Start tests for using Smol LM v2 model

--- PythonLibraries/HuggingFace/MoreTransformers/tests/integration_tests/Wrappers/Models/test_create_AutoModelForCausalLM.py
+++ PythonLibraries/HuggingFace/MoreTransformers/tests/integration_tests/Wrappers/Models/test_create_AutoModelForCausalLM.py
@@ -1,15 +1,40 @@
+from corecode.FileIO import is_directory_empty_or_missing
 from corecode.Utilities import DataSubdirectories
 
 from moretransformers.Wrappers.Models import create_AutoModelForCausalLM
 
+from pathlib import Path
+import pytest
+
 # TODO: Consider if BitsAndBytesConfig should be tested here.
 #from transformers import BitsAndBytesConfig, LlamaForCausalLM, LlamaModel
 
-from transformers import LlamaForCausalLM, LlamaModel
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    LlamaForCausalLM,
+    LlamaModel)
 
 import torch
 
 data_sub_dirs = DataSubdirectories()
+
+SMOL_V2_MODEL_DIR = data_sub_dirs.ModelsLLM / "HuggingFaceTB" / \
+    "SmolLM2-360M-Instruct"
+SMOL_V2_skip_reason = f"Directory {SMOL_V2_MODEL_DIR} is empty or doesn't exist"
+
+@pytest.mark.skipif(
+    is_directory_empty_or_missing(SMOL_V2_MODEL_DIR),
+    reason=SMOL_V2_skip_reason
+)
+def test_use_AutoModelForCausalLLM_with_SmolLMv2():
+    device = "cuda"
+    tokenizer = AutoTokenizer.from_pretrained(SMOL_V2_MODEL_DIR)
+    assert isinstance(tokenizer, PreTrainedTokenizerFast)
+    model = AutoModelForCausalLM.from_pretrained(SMOL_V2_MODEL_DIR).to(device)
+    assert isinstance(model, LlamaForCausalLM)
+    assert isinstance(model.model, LlamaModel)
+    assert model.device == torch.device(device, index=0)
 
 def test_create_AutoModelForCausalLM_instantiates_without_quantization():
     pretrained_model_path = data_sub_dirs.ModelsLLM / "meta-llama" / \
