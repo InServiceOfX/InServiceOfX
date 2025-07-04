8b23590db08f9dbac86004d671e31699c0538669
Ernest Yeung <ernestyalumni@gmail.com>
2025-07-04 14:03:30 -0700

    Restart SystemMessages testing

---
+++ PythonLibraries/ThirdParties/APIs/CommonAPI/tests/TestData/ExampleSystemMessages.py
@@ -0,0 +1,9 @@
+# https://github.com/dontriskit/awesome-ai-system-prompts?tab=readme-ov-file#
+class ExampleSystemMessages:
+    def __init__(self):
+        self.system_messages = [
+            {
+                "content": "You are a helpful assistant.",
+                "role": "system",
+            }
+        ]
