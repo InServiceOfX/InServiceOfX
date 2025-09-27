568e06c945ebb273edf5a1fbfa642f849ae77ead
Ernest Yeung <ernestyalumni@gmail.com>
2025-09-26 18:57:40 -0700

    Fixture setup is not working

--- PythonLibraries/Tools/tests/TestSetup/setup_PermanentConversation_RAG.py
+++ PythonLibraries/Tools/tests/TestSetup/setup_PermanentConversation_RAG.py
@@ -1,4 +1,11 @@
+from commonapi.Messages import (
+    ConversationSystemAndPermanent,
+    AssistantMessage,
+    UserMessage)
 from corecode.Utilities import DataSubdirectories, is_model_there
+from embeddings.TextSplitters import TextSplitterByTokens
+from sentence_transformers import SentenceTransformer
+
 from pathlib import Path
 
 from TestSetup.PostgreSQLDatabaseSetup import (
@@ -9,10 +16,14 @@ from TestSetup.PostgreSQLDatabaseSetup import (
     postgres_connection
 )
 
+from tools.RAG.PermanentConversation.EmbedPermanentConversation \
+    import EmbedPermanentConversation
+from tools.RAG.PermanentConversation import PostgreSQLInterface
+
 import json
 import pytest_asyncio
 
-def setup_PermanentConversation_RAG(
+def setup_PermanentConversation_RAG_dependences(
         test_database_name: str = "test_permanent_conversation_database"):
     data_subdirectories = DataSubdirectories()
     relative_model_path = "Models/Embeddings/BAAI/bge-large-en-v1.5"
@@ -23,29 +34,66 @@ def setup_PermanentConversation_RAG(
         f"Model {relative_model_path} not downloaded"
 
     postgresql_database_setup_data = PostgreSQLDatabaseSetupData()
-    @pytest_asyncio.fixture(scope="session")
-    def test_dsn():
-        return postgresql_database_setup_data.test_dsn
-
+    
     python_libraries_path = Path(__file__).parents[3]
 
     test_data_path = python_libraries_path / "ThirdParties" / "APIs" / \
         "CommonAPI" / "tests" / "TestData"
     test_conversation_path = test_data_path / "test_enable_thinking_true.json"
+    
     def load_test_conversation():
         with open(test_conversation_path, "r") as f:
             return json.load(f)
-
-    @pytest_asyncio.fixture(scope="function")
-    def test_db_name():
-        return test_database_name
 
     return (
         model_path,
         is_model_downloaded,
         model_is_not_downloaded_message,
-        test_dsn,
-        test_db_name,
+        test_database_name,
         load_test_conversation,
         cleanup_test_database,
         postgres_connection)
+
+async def setup_PermanentConversation_RAG():
+    model_path, is_model_downloaded, model_is_not_downloaded_message, \
+        test_db_name, load_test_conversation, \
+        cleanup_test_database, postgres_connection = \
+            setup_PermanentConversation_RAG_dependences()
+
+    await postgres_connection.create_database(test_db_name)
+    await postgres_connection.create_new_pool(test_db_name)
+    await postgres_connection.create_extension("vector")
+
+    pgsql_interface = PostgreSQLInterface(postgres_connection)
+    assert await pgsql_interface.create_tables() is True, \
+        "Tables should be created"
+
+    text_splitter = TextSplitterByTokens(model_path=model_path)
+    embedding_model = SentenceTransformer(str(model_path), device = "cuda:0",)
+    csp = ConversationSystemAndPermanent()
+    conversation = load_test_conversation()
+    for message in conversation:
+        if message["role"] == "user":
+            csp.append_message(UserMessage(message["content"]))
+        elif message["role"] == "assistant":
+            csp.append_message(AssistantMessage(message["content"]))
+        elif message["role"] == "system":
+            csp.add_system_message(message["content"])
+    embed_pc = EmbedPermanentConversation(
+        text_splitter,
+        embedding_model,
+        csp.pc)
+    message_chunks, message_pair_chunks = embed_pc.embed_conversation()
+
+    # Insert chunks into database.
+    for message_chunk in message_chunks:
+        await pgsql_interface.insert_message_chunk(message_chunk)
+    for message_pair_chunk in message_pair_chunks:
+        await pgsql_interface.insert_message_pair_chunk(message_pair_chunk)
+
+    return (
+        pgsql_interface,
+        embed_pc,
+        cleanup_test_database,
+        postgres_connection
+    )
--- PythonLibraries/Tools/tests/integration_tests/RAG/PermanentConversation/test_PermanentConversation_RAGAgent.py
+++ PythonLibraries/Tools/tests/integration_tests/RAG/PermanentConversation/test_PermanentConversation_RAGAgent.py
@@ -1,15 +1,39 @@
 from TestSetup.setup_PermanentConversation_RAG \
-    import setup_PermanentConversation_RAG
+    import (
+        setup_PermanentConversation_RAG,
+        setup_PermanentConversation_RAG_dependences,
+        )
 
 model_path, is_model_downloaded, model_is_not_downloaded_message, \
-    test_dsn, test_db_name, load_test_conversation, cleanup_test_database, \
+    test_db_name, load_test_conversation, cleanup_test_database, \
     postgres_connection = \
-        setup_PermanentConversation_RAG()
+        setup_PermanentConversation_RAG_dependences()
 
 from tools.Databases import PostgreSQLConnection
-from tools.RAG.PermanentConversation.RAGAgent import RAGAgent
+from tools.RAG.PermanentConversation import (
+    PostgreSQLInterface,
+    RAGAgent,
+    RAGProcessor,
+    )
 
 import pytest
+import pytest_asyncio
+
+@pytest_asyncio.fixture(scope="function")
+def test_db_name():
+    return "test_permanent_conversation_database"
+
+from TestSetup.PostgreSQLDatabaseSetup import (
+    # cleanup_test_database is run in postgres_connection, but you still need to
+    # import it so that postgres_connection can use it.
+    cleanup_test_database,
+    PostgreSQLDatabaseSetupData,
+    postgres_connection
+)
+postgresql_database_setup_data = PostgreSQLDatabaseSetupData()
+@pytest_asyncio.fixture(scope="session")
+def test_dsn():
+    return postgresql_database_setup_data.test_dsn
 
 @pytest.mark.asyncio
 async def test_PermanentConversation_RAGAgent_with_conversation(
@@ -17,6 +41,9 @@ async def test_PermanentConversation_RAGAgent_with_conversation(
     test_db_name: str,
     postgres_connection: PostgreSQLConnection):
 
-    await postgres_connection.create_database(test_db_name)
-    await postgres_connection.create_new_pool(test_db_name)
-    await postgres_connection.create_extension("vector")
+    pgsql_interface, embed_pc, cleanup_test_database, \
+        postgres_connection = await setup_PermanentConversation_RAG()
+
+    rag_processor = RAGProcessor(
+        pgsql_interface,
+        embed_pc)
