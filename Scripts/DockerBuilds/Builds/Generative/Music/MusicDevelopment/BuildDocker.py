16c942c91c6aebff8053004d94f912e53c4c0bc4
Ernest Yeung <ernestyalumni@gmail.com>
2025-11-30 09:17:55 -0800

    Start new Docker image for music

--- Scripts/DockerBuilds/Builds/Generative/Music/MusicDevelopment/BuildDocker.py
+++ Scripts/DockerBuilds/Builds/Generative/Music/MusicDevelopment/BuildDocker.py
@@ -3,7 +3,7 @@
 import sys
 from pathlib import Path
 
-docker_builds_directory = Path(__file__).resolve().parents[3]
+docker_builds_directory = Path(__file__).resolve().parents[4]
 
 if docker_builds_directory.exists():
     if str(docker_builds_directory) not in sys.path:
@@ -13,9 +13,7 @@ else:
         f"Docker builds directory {docker_builds_directory} does not exist")
 
 from Utilities import (BuildDockerBaseClass, BuildDockerCommand,)
-
 from Utilities.BuildDockerConfiguration import BuildDockerConfiguration
-
 from typing import List, Tuple
 
 class BuildDocker(BuildDockerBaseClass):
@@ -25,11 +23,11 @@ class BuildDocker(BuildDockerBaseClass):
         build_configuration = BuildDockerConfiguration.load_data(
             self._build_directory / "build_configuration.yml")
         super().__init__(
-            "Build Docker image for diffusers with minimal dependencies or at least no OpenCV",
+            "Build Docker image for full music development",
             build_configuration,
             self._build_directory,
             BuildDockerCommand(),
-            2)
+            3)
 
     def get_dockerfile_components(self) -> List[Tuple[str, Path]]:
         return [
@@ -40,14 +38,18 @@ class BuildDocker(BuildDockerBaseClass):
                 "Dockerfile.minimal_base",
                 self._parent_dir / "CommonFiles" / "Dockerfile.minimal_base"),
             (
-                "Dockerfile.pytorch_reinstall",
-                self._build_directory / "Dockerfile.pytorch_reinstall"),
+                "Dockerfile.cpp",
+                self._parent_dir / "CommonFiles" / \
+                    "Dockerfile.cpp_development"),
             (
-                "Dockerfile.nunchaku",
-                self._build_directory / "Dockerfile.nunchaku"),
+                "Dockerfile.transformers",
+                self._build_directory / "Dockerfile.transformers"),
             (
-                "Dockerfile.diffusers",
-                self._build_directory / "Dockerfile.diffusers"),
+                "Dockerfile.third_parties",
+                self._build_directory / "Dockerfile.third_parties"),
+            (
+                "Dockerfile.apis",
+                self._build_directory / "Dockerfile.third_parties"),
         ]
 
 def main():
