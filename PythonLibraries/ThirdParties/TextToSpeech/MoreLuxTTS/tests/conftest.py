50d988c48a6e97d9eb6d26f485e18d9337e8e62d
Ernest Yeung <ernestyalumni@gmail.com>
2026-01-27 05:34:04 -0800

    Adapt build for VibeVoice

--- PythonLibraries/ThirdParties/TextToSpeech/MoreLuxTTS/tests/conftest.py
+++ PythonLibraries/ThirdParties/TextToSpeech/MoreLuxTTS/tests/conftest.py
@@ -7,3 +7,13 @@ if corecode_path.exists() and corecode_path not in sys.path:
 	sys.path.append(str(corecode_path))
 elif not corecode_path.exists():
 	raise FileNotFoundError(f"CoreCode path {corecode_path} not found")
+
+from corecode.FileIO.get_project_directory_path import get_project_directory_path
+
+project_directory_path = get_project_directory_path()
+lux_tts_path = project_directory_path.parent / "ThirdParty" / "LuxTTS"
+
+if lux_tts_path.exists() and lux_tts_path not in sys.path:
+	sys.path.append(str(lux_tts_path))
+elif not lux_tts_path.exists():
+	raise FileNotFoundError(f"LuxTTS path {lux_tts_path} not found")
--- PythonLibraries/ThirdParties/TextToSpeech/MoreLuxTTS/tests/integration_tests/test_luxtts_usage.py
+++ PythonLibraries/ThirdParties/TextToSpeech/MoreLuxTTS/tests/integration_tests/test_luxtts_usage.py
@@ -24,8 +24,42 @@ if not example_sound_file_path.exists():
 from zipvoice.luxvoice import LuxTTS
 
 def test_luxtts_load_model():
-    lux_tts = LuxTTS
+    lux_tts = LuxTTS(model_path, device="cuda:0", threads=2)
+    # type(lux_tts): <class 'zipvoice.luxvoice.LuxTTS'>
+    #print("type(lux_tts):", type(lux_tts))
+    assert isinstance(lux_tts, LuxTTS)
 
 def test_luxtts_simple_inference():
+    lux_tts = LuxTTS(model_path, device="cuda:0", threads=2)
+
+    # ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'device', 'encode_prompt', 'feature_extractor', 'generate_speech', 'model', 'tokenizer', 'transcriber', 'vocos']
+    #print(dir(lux_tts))
+
+    # type(lux_tts.model): <class 'zipvoice.models.zipvoice_distill.ZipVoiceDistill'>
+    #print("type(lux_tts.model):", type(lux_tts.model))
+    # type(lux_tts.device): <class 'str'>
+    #print("type(lux_tts.device):", type(lux_tts.device))
+    assert lux_tts.device == "cuda:0"
+    # type(lux_tts.tokenizer): <class 'zipvoice.tokenizer.tokenizer.EmiliaTokenizer'>
+    #print("type(lux_tts.tokenizer):", type(lux_tts.tokenizer))
+    # type(lux_tts.transcriber): <class 'transformers.pipelines.automatic_speech_recognition.AutomaticSpeechRecognitionPipeline'>
+    #print("type(lux_tts.transcriber):", type(lux_tts.transcriber))
+    #type(lux_tts.vocos): <class 'linacodec.vocoder.vocos.Vocos'>
+    #print("type(lux_tts.vocos):", type(lux_tts.vocos))
 
     text = "Hey, what's up? I'm feeling really great if you ask me honestly!"
+    encoded_prompt = lux_tts.encode_prompt(example_sound_file_path, rms=0.01)
+
+    print("type(encoded_prompt):", type(encoded_prompt))
+
+    num_steps = 256
+
+    final_wav = lux_tts.generate_speech(text, encoded_prompt, num_steps)
+
+    print("type(final_wav):", type(final_wav))
+
+    audio_data = final_wav.numpy().squeeze()
+    sampling_rate = 48000
+    sf.write("test_luxtts_simple_inference.wav", audio_data, sampling_rate)
+
+    assert True
